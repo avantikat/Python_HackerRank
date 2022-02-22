f5ab4f40895bf2a4acfc827765c281584468e4d4
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2022-02-22 19:55:49 +0530

    Create List_Comprehensions.py

---
+++ List_Comprehensions.py
@@ -0,0 +1,13 @@
+x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    output = [];
+    abc = [];
+    for X in range(x+1):
+        for Y in range(y+1):
+            for Z in range(z+1):
+                if X+Y+Z != n:
+                    abc = [X,Y,Z];
+                    output.append(abc);
+print(output);
